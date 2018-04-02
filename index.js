93c66713924b24dc3bf72622d6ffbccd7c060739
Ubuntu <arman@AIJobDispatcher.dggjdzq41x3e5ci4xqilxovfff.bx.internal.cloudapp.net>
2018-04-02 02:47:06 +0000

    Implemented DICOM parsing of the incoming files and storing in separate folders named by Study UIDs

--- index.js
+++ index.js
@@ -1,34 +1,49 @@
-const http = require("http");
-const axios = require("axios");
-const fs = require("fs");
+const http = require('http');
+const axios = require('axios');
+const fs = require('fs-extra');
+const dicom = require('dicom-parser/dist/dicomParser');
 
-const hostname = "0.0.0.0";
+const hostname = '0.0.0.0';
 const port = 3000;
 
 const server = http.createServer((req, res) => {
   console.log(`\n${req.method} ${req.url}`);
   console.log(req.headers);
 
-  req.on("data", function(chunk) {
+  req.on('data', function(chunk) {
     response = JSON.parse(chunk);
     //console.log("BODY: " + chunk);
-  
+
     response.uris.map(url => {
-      axios.get(url, {
-        responseType: 'arraybuffer',
-      })
-      .then(result => {
-	 const num = Math.floor((Math.random() * 1000) + 1);
-         const outputFilename = `./file${num}.dcm`;
-         fs.writeFileSync(outputFilename, result.data);
-         console.log(`Wrote file ./file${num}.dcm`);
-      });
+      axios
+        .get(url, {
+          responseType: 'arraybuffer'
+        })
+        .then(result => {
+          try {
+            const { studyUid, imageUid } = getUids(result.data);
+            const outputFilename = `./tmp/${studyUid}/${imageUid}.dcm`;
+            fs.ensureDirSync(`./tmp/${studyUid}`);
+
+            fs.writeFileSync(outputFilename, result.data);
+            console.log(`Wrote file ${outputFilename}`);
+          } catch (err) {
+            console.error(err);
+          }
+        });
     });
   });
   res.statusCode = 200;
-  res.setHeader("Content-Type", "text/plain");
-  res.end("Acknowledged\n");
+  res.setHeader('Content-Type', 'text/plain');
+  res.end('Acknowledged\n');
 });
+
+const getUids = dicomData => {
+  const dataSet = dicom.parseDicom(dicomData);
+  const studyUid = dataSet.string('x0020000d');
+  const imageUid = dataSet.string('x00080018');
+  return { studyUid, imageUid };
+};
 
 server.listen(port, hostname, () => {
   console.log(`Server running at http://localhost:${port}/`);
