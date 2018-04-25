19e84a2203fed2bcd33b0e56ccc22ebc1558354d
Arman Sharafshahi <asharafshahi@gmail.com>
2018-04-25 17:01:12 -0400

    added DICOM initiated mode

--- index.js
+++ index.js
@@ -4,16 +4,26 @@ const FormData = require('form-data');
 const fs = require('fs-extra');
 const util = require('util');
 const exec = util.promisify(require('child_process').exec);
+const writeFilePromise = util.promisify(fs.writeFile);
 const dicom = require('dicom-parser/dist/dicomParser');
+const aiMktApi = require('@nuance/ai-marketplace-api');
 require('dotenv').load();
 
 const imageRootDir = process.env.IMAGE_ROOT_DIR;
 const AiMktPlaceAPI = process.env.AI_TRANSACTIONS_ENDPOINT;
+const serviceId = process.env.AI_SERVICE_ID;
 const serviceKey = process.env.SERVICE_KEY;
 const hostname = '0.0.0.0';
 const port = 3000;
+const preProcessCmd = process.env.EXTERNAL_PREPROCESS_CMD;
+const preProcessDir = process.env.PRE_PROCESS_OUTPUT_DIR;
+const modelOutputDir = process.env.MODEL_OUTPUT_DIR;
+const postProcessDir = process.env.POST_PROCESS_OUTPUT_DIR;
+const postProcessCmd = process.env.EXTERNAL_POSTPROCESS_CMD;
 const modelEndpoint = 'http://127.0.0.1:8000/score/?file=';
-
+const aiTransactions = new aiMktApi(process.env.AI_TRANSACTIONS_ENDPOINT, 
+  process.env.AI_TRANSACTIONS_KEY)
+  
 const server = http
   .createServer((req, res) => {
     console.log(`\n${req.method} ${req.url}`);
@@ -25,30 +35,11 @@ const server = http
       })
       .on('end', async () => {
         body = Buffer.concat(body).toString();
-        const { transactionId, uris } = JSON.parse(body);
-        let studyFolder;
-        await Promise.all(
-          uris.map(async url => {
-            try {
-              const result = await axios.get(url, {
-                responseType: 'arraybuffer'
-              });
-              const { studyUid, imageUid } = getUids(result.data);
-              studyFolder = `${imageRootDir}/${studyUid}`;
-              const outputFilename = `${imageRootDir}/${studyUid}/${imageUid}.dcm`;
-              fs.ensureDirSync(studyFolder);
-              fs.writeFileSync(outputFilename, result.data);
-              console.log(`Wrote file ${outputFilename}`);
-              await preProcessToPng(studyFolder);
-            } catch (err) {
-              console.error(err);
-            }
-          })
-        );
-        const result = await runModel(studyFolder + '/preprocess');
-        console.log(`AI model returned: ${result[0].data}`);
-        const postProcessedData = postProcessToJson(result);
-        await postToAIMarketplace(transactionId, postProcessedData);
+        if (req.url === '/') {
+          handleAIMarketplaceRequest(body);
+        } else if (req.url === '/dicomInitiated') {
+          handleDicomInitiatedRequest(body);
+        }
       });
     res.statusCode = 200;
     res.setHeader('Content-Type', 'text/plain');
@@ -57,6 +48,54 @@ const server = http
   .listen(port, hostname, () => {
     console.log(`Server running at http://localhost:${port}/`);
   });
+
+const handleDicomInitiatedRequest = async reqBody => {
+  try {
+    const { studyUID, studyFolder } = JSON.parse(reqBody);
+    // pre-process 
+    await exec(`${preProcessCmd} studyFolder ${preProcessDir}/${studyUID}`);
+    // process all files through model, returns array of results
+    const result = await runModel(`${preProcessDir}/${studyUID}`);
+    await writeFilePromise(`${modelOutputDir}/${studyUID}/result.csv`, result.join(), 'utf8');
+    // post-process results
+    await exec(`${postProcessCmd} ${modelOutputDir}/${studyUID}/result.csv` `${postProcessDir}/${studyUID}`);
+    const transactionId = await aiTransactions.createTransaction(serviceId, studyUID, '0000000');
+    await aiTransactions.uploadResult(transactionId, serviceKey, 'test', final)
+  } catch (err) {
+    console.error(err);
+  }
+}
+
+const handleAIMarketplaceRequest = async reqBody => {
+  try {
+    const { transactionId, uris } = JSON.parse(reqBody);
+    let studyFolder;
+    await Promise.all(
+      uris.map(async url => {
+        try {
+          const result = await axios.get(url, {
+            responseType: 'arraybuffer'
+          });
+          const { studyUid, imageUid } = getUids(result.data);
+          studyFolder = `${imageRootDir}/${studyUid}`;
+          const outputFilename = `${imageRootDir}/${studyUid}/${imageUid}.dcm`;
+          fs.ensureDirSync(studyFolder);
+          fs.writeFileSync(outputFilename, result.data);
+          console.log(`Wrote file ${outputFilename}`);
+          await preProcessToPng(studyFolder);
+        } catch (err) {
+          console.error(err);
+        }
+      })
+    );
+    const result = await runModel(studyFolder + '/preprocess');
+    console.log(`AI model returned: ${result[0].data}`);
+    const postProcessedData = postProcessToJson(result);
+    await aiTransactions.uploadResult(transactionId, serviceKey, 'test', postProcessedData);
+  } catch (err) {
+    console.error(err);
+  }
+}
 
 const getUids = dicomData => {
   const dataSet = dicom.parseDicom(dicomData);
@@ -104,30 +143,3 @@ const postProcessToJson = allResults =>
       { findings: [] }
     )
   );
-
-// This function may need to be customized if more than just JSON results are returned
-const postToAIMarketplace = async (transactionId, data) => {
-  let url = `${AiMktPlaceAPI}/${transactionId}/results`;
-  const body = {
-    serviceKey,
-    resultKey: 'test'
-  };
-  const response = await axios.post(url, body);
-  const resultId = response.data.result.id;
-  url = `${url}/${resultId}/documents`;
-  const config = {
-    headers: {
-      'content-type': 'multipart/form-data'
-    }
-  };
-  fs.writeFileSync('result.json', data, 'utf8');
-  form = new FormData();
-  form.append('documentType', 'json');
-  form.append('name', 'AI result');
-  form.append('file', fs.createReadStream('result.json'));
-  console.log(url);
-  form.submit(url, (err, res) => {
-    if (!err) console.log('submission success');
-    fs.unlinkSync('result.json');
-  });
-};
